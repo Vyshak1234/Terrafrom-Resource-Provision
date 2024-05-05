6d47ceb6909da8db88244f9bcf2e453cb1859bb5
Vyshak1234 <124694559+Vyshak1234@users.noreply.github.com>
2024-05-05 15:33:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "us-east-2"
 }
-
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+resource "aws_instance" "testec2" {
+    ami = "ami-0ddda618e961f2270"
+    instance_type = "t2.micro"
+  
 }
